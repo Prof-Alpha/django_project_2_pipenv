2c1f6a59e2fdf5fbcaddc611c46d23b8679a75c9
prof-alpha <saeed26x@yahoo.com>
2021-02-19 17:13:04 +0330

    template and static updated

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="blog_app/first_page.html"), name='blog_first_page' ),
+    path('', TemplateView.as_view(template_name="blog_app/blog.html"), name='blog_first_page' ),
+    path('single-post/', TemplateView.as_view(template_name="blog_app/single_post.html"), name='blog_first_page' ),
+    path('register/', TemplateView.as_view(template_name="blog_app/register.html"), name='blog_first_page' ),
 ]
